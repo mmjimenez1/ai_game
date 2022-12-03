5e52df5fa858e2da35a92e93d10cac119367e3e9
Melanie Jimenez <mmjimenez@owu.edu>
2022-12-03 18:04:34 -0500

    add enemy manager

--- Scripts/UtilityAI/Core/EnemyManager.cs
+++ Scripts/UtilityAI/Core/EnemyManager.cs
@@ -1,8 +1,24 @@
-﻿using System;
-public class NewClass
-{
-	public NewClass()
-	{
-	}
-}
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+
+public class EnemyManager: MonoBehaviour
+{
+    public AIBrain brain { get; set; }
+    public Action[] possibleActions;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        brain = GetComponent<AIBrain>();
+    }
+
+
+
+
+}
